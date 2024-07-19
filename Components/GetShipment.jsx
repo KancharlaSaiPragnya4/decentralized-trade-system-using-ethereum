8e35ff67cab8db08d18c69d861b038cd1c21a106
pragnya <pragnyareddy9241@gmail.com>
2024-07-19 10:12:33 +0530

    Initial commit

---
+++ Components/GetShipment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetShipment = ({ getModel, setGetModel, getShipment }) => {
+  return (
+    <div>
+      GetShipment
+    </div>
+  )
+}
+
+export default GetShipment;
